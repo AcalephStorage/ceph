f214bde574b01ca8443c46edeb5ebc4793eef99b
Julio Telan <julio_telan@dlsu.edu.ph>
2015-03-26 16:55:26 +0800

    Update to the latest version of ceph source codes (v0.92)

--- src/include/unordered_map.h
+++ src/include/unordered_map.h
@@ -9,16 +9,15 @@
 
 namespace ceph {
   using std::unordered_map;
+  using std::unordered_multimap;
 }
-
 #else
 
 #include <tr1/unordered_map>
-
 namespace ceph {
   using std::tr1::unordered_map;
+  using std::tr1::unordered_multimap;
 }
-
 #endif
 
 #endif
