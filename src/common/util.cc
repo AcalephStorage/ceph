6a2abf45d8c6a4fd56066b71883b5a55e039c7ab
Julio Telan <julio_telan@dlsu.edu.ph>
2015-03-31 14:50:56 +0800

    Added #ifndef _WIN32 guards for libraries not available in windows environment

--- src/common/util.cc
+++ src/common/util.cc
@@ -17,11 +17,12 @@
 #include "include/util.h"
 #include "common/errno.h"
 #include "common/strtol.h"
-
+#ifdef _WIN32
+#else
 #ifdef HAVE_SYS_VFS_H
 #include <sys/vfs.h>
 #endif
-
+#endif
 // test if an entire buf is zero in 8-byte chunks
 bool buf_is_zero(const char *buf, size_t len)
 {
